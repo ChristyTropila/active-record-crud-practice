d3c3dfcdd18e2cf3b5a68d4a95c3114765900f45
ChristyTropila <tropila04@gmail.com>
2020-08-13 13:36:44 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_202620) do
+ActiveRecord::Schema.define(version: 2020_08_13_150820) do
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "plants", force: :cascade do |t|
     t.string "name"
